7e0ae1592bc9560cb2ce94d311608d0d20de2d68
Roman Kojnok <roman.kojnok@protonmail.com>
2022-09-15 14:44:31 +0100

    Initial commit

---
+++ jenkins-install.sh
@@ -0,0 +1,6 @@
+docker pull jenkins/jenkins:lts
+
+docker run -d --name jenkins -p 8080:8080 jenkins/jenkins:lts
+
+# Initial run
+docker exec -it jenkins cat /var/jenkins_home/secrets/initialAdminPassword
