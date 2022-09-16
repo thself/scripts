da7dd62d32383d950229af74a974af22135311f5
Roman Kojnok <roman.kojnok@protonmail.com>
2022-09-16 12:43:47 +0100

    first commit

---
+++ post-install-docker.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+sudo docker run hello-world
+
+echo "Done!"
+
+sudo systemctl enable docker.service
+
+echo "docker.service enabled!"
+
+sudo systemctl enable containerd.service
+
+echo "containerd.service enabled!"
+echo "Finished!"
