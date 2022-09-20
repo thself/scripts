f7180a3c6091b2ab399d30fb247a7688e715a68d
roman-kojnok <110659170+roman-kojnok@users.noreply.github.com>
2022-09-20 16:57:41 +0100

    Update azure-ubuntu-jenkins.sh

--- azure-ubuntu-jenkins.sh
+++ azure-ubuntu-jenkins.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # run 'sudo su' before executing this script
 sudo apt update
-sudo apt install openjdk-11-jdk
+sudo apt install openjdk-11-jdk -y
 java -version
 wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
 sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
 sudo apt update
-sudo apt install jenkins
+sudo apt install jenkins -y
 # if Error: W: GPG error: https://pkg.jenkins.io/debian-stable binary/ Release: 
 #The following signatures couldn’t be verified because the public key is not available: NO_PUBKEY 9B7D32F2D50582E6"
 #sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 9B7D32F2D50582E6
