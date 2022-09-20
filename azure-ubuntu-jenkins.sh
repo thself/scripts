8e936a4077a4f0acb742f25e044fc16a73e8f797
roman-kojnok <110659170+roman-kojnok@users.noreply.github.com>
2022-09-20 15:22:39 +0100

    Update azure-ubuntu-jenkins.sh

--- azure-ubuntu-jenkins.sh
+++ azure-ubuntu-jenkins.sh
@@ -1,20 +1,3 @@
 #!/bin/bash
-sudo apt update
-sudo apt install openjdk-11-jre
-echo
-echo
-java -version
-echo
-echo
-curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
-echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
-sudo apt-get install jenkins
-echo
-echo
-echo "Installation succesful!"
-echo
-sudo systemctl enable jenkins
-sudo systemctl start jenkins
-sudo systemctl status jenkins > /dev/null
-sudo cat /var/lib/jenkins/secrets/initialAdminPassword
-echo "<<<copy the string above and paste it to your browser>>>"
+Installation problem!
+Code soon!
