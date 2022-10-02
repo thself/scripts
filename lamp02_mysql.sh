6fa4c57075adbe2575c25c2a738506cd4837f4b1
roman-kojnok <110659170+roman-kojnok@users.noreply.github.com>
2022-10-02 12:58:45 +0100

    Update lamp02_mysql.sh

--- lamp02_mysql.sh
+++ lamp02_mysql.sh
@@ -7,3 +7,5 @@ echo "sudo mysql_secure_installation"
 echo "More Info:"
 echo "https://www.digitalocean.com/community/tutorials/how-to-install-linux-apache-mysql-php-lamp-stack-on-ubuntu-20-04"
 echo
+# mysql> CREATE USER 'wordpress'@'localhost' IDENTIFIED WITH mysql_native_password BY '{superSecretPassword!123}';
+# mysql> GRANT ALL ON `wordpress`.* TO 'wordpress'@'localhost';
