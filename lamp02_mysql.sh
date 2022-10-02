2ccdbec4a47e7a04f0f9a7a249b6016f1b763cb2
roman-kojnok <110659170+roman-kojnok@users.noreply.github.com>
2022-10-02 12:40:42 +0100

    Update lamp02_mysql.sh

--- lamp02_mysql.sh
+++ lamp02_mysql.sh
@@ -1,10 +1,9 @@
 sudo apt install mysql-server -y
-# sudo mysql_secure_installation
 echo
 echo "sudo mysql"
 echo "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'SetRootPasswordHere';"
-# mysql> exit
-sudo mysql_secure_installation
+echo mysql> exit
+echo "sudo mysql_secure_installation"
 echo "More Info:"
 echo "https://www.digitalocean.com/community/tutorials/how-to-install-linux-apache-mysql-php-lamp-stack-on-ubuntu-20-04"
 echo
